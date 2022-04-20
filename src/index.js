5c600e88e9230b6d5bcb192e70b62b060c02204a
Dritriy Golyshev <4dimariK@gmail.com>
2022-04-20 08:18:29 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,35 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
-import { titleChanged, taskDeleted, completeTask } from "./store/task";
+import {
+  titleChanged,
+  taskDeleted,
+  completeTask,
+  loadTasks,
+  getTasks,
+  getTasksLoadingStatus,
+  createTasks,
+} from "./store/task";
+import { Provider, useSelector, useDispatch } from "react-redux";
+import { getError } from "./store/errors";
 
 const store = configureStore();
 
-const App = (params) => {
-  const [state, setState] = useState(store.getState());
+const App = () => {
+  const state = useSelector(getTasks());
+  const isLoading = useSelector(getTasksLoadingStatus());
+  const error = useSelector(getError());
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    store.subscribe(() => {
-      setState(store.getState());
-    });
+    dispatch(loadTasks());
   }, []);
 
   const changeTitle = (taskId) => {
-    store.dispatch(titleChanged(taskId));
+    dispatch(titleChanged(taskId));
   };
   const deleteTask = (taskId) => {
-    store.dispatch(taskDeleted(taskId));
+    dispatch(taskDeleted(taskId));
   };
+
+  const addNewTask = () => {
+    dispatch(createTasks({ userId: 1, title: "NEW TASK", completed: false }));
+  };
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <>
       <h1>App</h1>
-
+      <button onClick={() => addNewTask()}>add Task</button>
       <ul>
         {state.map((el) => (
           <li key={el.id}>
             <p>{el.title}</p>
             <p>{`Completed: ${el.completed}`}</p>
-            <button onClick={() => store.dispatch(completeTask(el.id))}>
+            <button onClick={() => dispatch(completeTask(el.id))}>
               Completed
             </button>
             <button onClick={() => changeTitle(el.id)}>ChangeTitle</button>
@@ -43,7 +67,9 @@ const App = (params) => {
 };
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/store/task.js
+++ src/store/task.js
@@ -1,28 +1,77 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import todosService from "../todos.service";
+import { setError } from "./errors";
 
-const initialState = [
-  { id: 1, title: "Task 1", completed: false },
-  { id: 2, title: "Task 2", completed: false },
-];
+const initialState = { entities: [], isLoading: true };
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
+    received(state, action) {
+      state.entities = action.payload;
+      state.isLoading = false;
+    },
     update(state, action) {
-      const elIndex = state.findIndex((el) => el.id === action.payload.id);
-      state[elIndex] = { ...state[elIndex], ...action.payload };
+      const elIndex = state.entities.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      state.entities[elIndex] = {
+        ...state.entities[elIndex],
+        ...action.payload,
+      };
     },
     remove(state, action) {
-      return state.filter((el) => el.id !== action.payload.id);
+      state.entities = state.entities.filter(
+        (el) => el.id !== action.payload.id
+      );
+    },
+    loadTasksRequested(state) {
+      state.isLoading = true;
+    },
+
+    taskRequestFailed(state) {
+      state.isLoading = false;
+    },
+    taskAdded(state, action) {
+      state.entities.push(action.payload);
     },
   },
 });
 
 const { actions, reducer: taskReducer } = taskSlice;
-const { update, remove } = actions;
+const {
+  update,
+  remove,
+  received,
+  loadTasksRequested,
+  taskRequestFailed,
+  taskAdded,
+} = actions;
 
-export const completeTask = (id) => (dispatch, getState) => {
+const taskRequested = createAction("task/taskRequested");
+
+export const loadTasks = () => async (dispatch) => {
+  dispatch(loadTasksRequested());
+  try {
+    const data = await todosService.fetch();
+    dispatch(received(data));
+  } catch (e) {
+    dispatch(taskRequestFailed());
+    dispatch(setError(e.message));
+  }
+};
+export const createTasks = (task) => async (dispatch) => {
+  dispatch(taskRequested());
+  try {
+    const data = await todosService.create(task);
+    dispatch(taskAdded(data));
+  } catch (e) {
+    dispatch(taskRequestFailed());
+    dispatch(setError(e.message));
+  }
+};
+export const completeTask = (id) => (dispatch) => {
   dispatch(update({ id, completed: true }));
 };
 
@@ -33,5 +82,8 @@ export function titleChanged(id) {
 export function taskDeleted(id) {
   return remove({ id });
 }
+
+export const getTasks = () => (state) => state.tasks.entities;
+export const getTasksLoadingStatus = () => (state) => state.tasks.isLoading;
 
 export default taskReducer;
